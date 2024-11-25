66415809aac5cd8926714372f9c8ecefcd327da7
CodeExia <lunaticpsyker77@gmail.com>
2024-11-26 00:13:18 +0800

    HAHAHA I DID IT

--- revisedtwosum.c
+++ revisedtwosum.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int i;
-int g = 0;
+int k;
 
 int num [] = {2, 7, 11, 15};
 int target; 
@@ -11,13 +11,13 @@ int main()
     printf("What is your targeted number?\n");
     scanf("%d", &target);
 
-    for (int g = 0; g < 4; ++g)
+    for (int k = 0; k < 4; ++k)
     {
         for (int i = 0; i < 4; ++i)
         {
-            if (num[g] + num[i + 1] == target)
+            if (num[k] + num[i + 1] == target)
             {
-                printf("%d and %d\n", num[g], num[i + 1]);
+                printf("%d and %d\n", num[k], num[i + 1]);
                 return 0;
             }
         }
