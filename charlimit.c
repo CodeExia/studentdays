84326ce0ed33e5c7542b770ea419f9168748366f
CodeExia <lunaticpsyker77@gmail.com>
2025-01-11 18:29:28 +0800

    Review

---
+++ charlimit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char college[5] = "CCMIT";
+
+    printf("%c\n", college[0]);
+    printf("%c\n", college[1]);
+    printf("%c\n", college[2]);
+    printf("%c\n", college[3]);
+    printf("%c\n", college[4]);
+    printf("%c\n", college[5]);
+    
+    printf("%s\n", college);
+    return 0;
+}
---
+++ sortingfirsttry.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int unsorted[] = {123,53,7,65,3,213,756,897,455,567,876,234,325,200,100,321,213};
+    int i,f,temp;
+
+    for (int i = 0; i < 17; i++)
+    {
+        for (int f = 0; f < 17; f++)
+        {
+            if (unsorted[i] < unsorted[f])
+            {
+                temp = unsorted[i];
+                unsorted[i] = unsorted[f];
+                unsorted[f] = temp;
+            }
+            else if (unsorted[i] > unsorted[f])
+                printf("Index: %d is already sorted\n", unsorted[i]);
+        }
+    }
+
+    for (int print = 0; print < 17; print++)
+    {
+        printf("%d ", unsorted[print]);
+    }
+    return 0;
+
+}
