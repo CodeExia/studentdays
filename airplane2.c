349f7bef4ae6eeead775fd4b737737342312a38f
Ezra <49109859+CodeExia@users.noreply.github.com>
2025-05-22 08:37:25 +0800

    Add files via upload

---
+++ airplane2.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+
+struct seats{
+	int row;
+	char column;
+	int availability;
+}seat[5][4];
+
+void displaySeats(struct seats seat[5][4]); //Passing the whole structure
+void initialization(struct seats array[5][4]); //Initialize passing the whole structure as array
+int errorChecking(int avail); //Passing individual member
+void displaySeatsPtr(struct seats *seat); //Passing the struct as pointer
+
+int columnToIndex(char col);
+int main()
+{
+
+	int decision = 1;
+	char input[10];
+	int counter= 0; //The counter to full seats.
+
+	//Loop to intializae the default, avaialble values. While it is possible to make a matrix or declare them manually, that's over 20 same operations.
+	initialization(seat);
+		
+	while (counter < 20) //5x4, pretty self explanatory
+	{
+
+		displaySeatsPtr(&seat);
+		//Displays the current matrix of seats using Pointer.
+		//This casts the 2d struct array into 1d.
+		int row;
+		char col;
+		
+		//If you want to end the program early
+		printf("Do you want to assign a seat? (1 for Yes, anything else for No)\n");
+		scanf("%d", &decision);
+			if (decision != 1)
+				break;
+		
+		//Column and row input
+		printf("\nEnter which row:\n");
+		scanf("%d", &row);
+		int indexRow = row - 1; //Since its converting to index, we need to start at 0.
+		
+		printf("\nEnter which column:\n");
+		scanf(" %c", &col);
+		int indexCol = columnToIndex(col);
+		//Calls the function to convert the letter into an index number for our struct
+		
+		if (row < 1 || row > 5 || indexCol == -1)
+		{
+			printf("Seat doesn't exist. Please try again.\n");
+			continue;
+		}
+		
+		if (errorChecking(seat[indexRow][indexCol].availability)) //Remember, the default value is 0
+			printf("Seat %d%c is already taken, please choose another seat.\n", row, col);
+		else 
+		{
+			seat[indexRow][indexCol].availability = 1;
+			counter++;
+			printf("Seat %d%c has been assigned.\n", row, col);
+		}	
+	}
+	
+	printf("\nFinal seating arrangment:\n");
+	displaySeats(seat);
+	//Passing the whole structure into a function that prints it.
+	
+	return 0;
+}
+
+void initialization(struct seats array[5][4])
+{
+	for (int i = 0; i < 5; i++)
+		for (int j = 0; j < 4; j++)
+		{
+			array[i][j].row = i+1; //Plus one since the output needs to start at 1
+			array[i][j].column = 'A' + j; //Increments from A to D. ASSCI are just ints read as characters.
+			array[i][j].availability = 0; //By default, the seats are avaiblble.
+		}
+}
+
+int columnToIndex(char col)
+{
+	if (col >= 'A' && col <= 'D') //To isolate the inputs only from A to D.
+		return col - 'A'; //Since 'A' is the start of the uppercase ASCCI characters, subtracting it from the col just ensures the index is 0-3.
+	return -1;
+}
+
+//Returns 1 if the availability is 1
+int errorChecking(int avail)
+{
+	if (avail == 1)
+		return 1;
+	else 
+		return 0;
+}
+
+void displaySeats(struct seats seat[5][4])
+{
+	printf("\nSeat availability:\n");
+	for (int i = 0; i < 5; i++)
+	{
+		printf("%d\t", i+1); //This is the initial numbers. +1 so it doesn't start from zero.
+		for (int j = 0; j < 4; j++)
+		{
+			if (seat[i][j].availability) //It's just a switch. If its 1 or occupied, then it returns a big X.
+				printf("X\t");
+			else 
+				printf("%c\t", seat[i][j].column);
+		}
+		printf("\n"); //Just an escape character to the next line
+	}
+}
+
+void displaySeatsPtr(struct seats *e)
+{
+	printf("\nSeat availability:\n");
+	for (int i = 0; i < 5; i++)
+	{
+		printf("%d\t", i+1); //This is the initial numbers. +1 so it doesn't start from zero.
+		for (int j = 0; j < 4; j++)
+		{
+			if ((e +i*4+j)->availability) //It's just a switch. If its 1 or occupied, then it returns a big X.
+				printf("X\t");
+			else 
+				printf("%c\t", (e +i*4+j)->column);
+		}
+		printf("\n"); //Just an escape character to the next line
+	}
+}
+
+void displaySeatsArr(struct seats a[5][4])
+{
+	printf("\nSeat availability:\n");
+	for (int i = 0; i < 5; i++)
+	{
+		printf("%d\t", i+1); //This is the initial numbers. +1 so it doesn't start from zero.
+		for (int j = 0; j < 4; j++)
+		{
+			if (a[i][j].availability) //It's just a switch. If its 1 or occupied, then it returns a big X.
+				printf("X\t");
+			else 
+				printf("%c\t", a[i][j].column);
+		}
+		printf("\n"); //Just an escape character to the next line
+	}
+}
+
+
+
---
+++ poker2.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+
+//Static and const to ensure it never accidentally gets changed during the runtime.
+static const int numberCards = 5;
+static const int maxVal = 13; //There are only 13 possible values
+
+struct hand{
+	char suit;
+	int value;
+	
+}card[5];
+
+void printHand(struct hand current[]); //Pass the whole structure and print
+void sort(struct hand card[]); //Sorting function by passing the structure array
+void sortPtr(struct hand *ptr); //Sorting function using a pointer
+int straightFunc(int first, int second); //Pass the member to determine the straightness
+
+
+int main()
+{
+	int decision = 1;
+	while (decision == 1)
+	{
+	printf("What is your hand?\n");
+	int counts[13] = {0};
+	int pair = 0, three = 0, four = 0;
+	//Switches for flush or straight results
+	int flush = 1; //Flush is true by default, if it becomes zero later on it means it's not a flush
+	int straight = 1; //This is also true by default so by the cheking it becomes zero
+	
+	for (int i = 0; i < numberCards; i++)
+	{
+		printf("Card[%d]'s suit(C D H S):\n", i+1);
+		scanf(" %c", &card[i].suit);
+		//the space before the %c helps to not take \n as the char
+		printf("Card[%d]'s value(2-14, 14 = Ace):\n", i+1);
+		//It starts at 2 so when we have to subtract it by 2, the index would start at 0.
+		scanf("%d", &card[i].value);
+		
+		//This is to count the number of times each value has appeared
+		int index = card[i].value - 2;
+        if (index < 0 || index >= maxVal) index = 0; //There are only 13 possible card values. Also to check if it goes out of bounds.
+        	counts[index]++;
+		
+		//Flush checking. This is to see if they all have the same suits
+		if (i > 0 && card[i].suit != card[0].suit)
+			flush = 0;
+	}
+	
+	//Sorting using pointer
+	sortPtr(&card);
+	//Sorting using whole structure
+	sort(card);
+	
+	
+	for (int i = 0; i < numberCards-1; i++)
+	{
+		if (straightFunc(card[i].value, card[i+1].value) != 1)
+		{
+			straight = 0;
+			break;
+		}
+	}
+	
+	//Checking for low ace
+	if (card[0].value == 2 && card[1].value == 3 && card[2].value == 4 && card[3].value == 5 && card[4].value == 14)
+        straight = 1;
+	
+	//Remember that the array for counts is incremented everytime it gets a card of the same value.
+	for (int i = 0; i < maxVal; i++)
+	{
+		if (counts[i] == 2)
+			pair++;
+		if (counts[i] == 3)
+			three++;
+		if (counts[i] == 4)
+			four++;
+	}
+	
+	printf("\nYour current hand is:\n");
+	printHand(card);
+	
+	
+	//Since every switch that needed to be checked is now done, we score now in the order of highest ranking to lowest.
+	if (straight && flush)
+		printf("Straight flush.\n");
+	else if (four)
+		printf("Four of a kind.\n");
+	else if (three && pair)
+		printf("Full house.\n");
+	else if (flush)
+		printf("Flush.\n");
+	else if (straight)
+		printf("Straight.\n");
+	else if (three)
+		printf("Three of a kind.\n");
+	else if (pair == 2)
+		printf("Two pairs.\n");
+	else if (pair == 1)
+		printf("One pair.\n");
+	else
+		printf("Nothing.\n");
+	//If none of the above triggers, it's just nothing.
+	
+	printf("Do you want to try another hand? (1 for Yes, anything else for No)\n");
+	scanf("%d", &decision);
+	}
+	return 0;
+}
+
+void sort(struct hand card[])
+{
+	//Sorting for easier straight checking
+	for (int i = 0; i < numberCards-1; i++)
+		for (int j = 0; j < numberCards-1; j++)
+			if (card[j].value > card[j+1].value)
+			{
+				struct hand temp = card[j];
+				card[j] = card[j+1];
+				card[j+1] = temp;
+			}
+}
+
+void sortPtr(struct hand *ptr)
+{
+	//Sorting for easier straight checking
+	for (int i = 0; i < numberCards-1; i++)
+		for (int j = 0; j < numberCards-1; j++)
+			if ((ptr+j)->value > (ptr+j+1)->value)
+			{
+				struct hand temp = *(ptr+j);
+				*(ptr+j) = *(ptr+j+1);
+				*(ptr+j+1) = temp;
+			}
+}
+
+void printHand(struct hand current[])
+{
+	for (int i = 0; i < 5; i++)
+		printf("%d%c\t", current[i].value, current[i].suit);
+	printf("\n");
+}
+
+int straightFunc(int first, int second)
+{
+	int result = second - first;
+	return result;
+}
